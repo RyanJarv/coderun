f21d3d4dc8ce74bf42c5fe43b7efa93d06d338cf
RyanJarv <ryan_gerstenkorn@fastmail.fm>
2020-11-22 23:47:22 -0600

    Remove unneeded test files for now

--- test/test.js
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env node
-
-console.log("Hello from nodejs");
-
-exports.lambda_handler = function(event, context, callback) {
-  console.log("Hello from lambda");
-}
--- test/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-print("hello from python")
-
-import awscli
-print(dir(awscli))
-
-def lambda_handler(a, b):
-  print("hello from lambda")
-  print(dir(awscli))
